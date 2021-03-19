e2e031530b2d13c86d145196d2ca6393cceee209
Logan Harris <loganrodharris@gmail.com>
2021-03-19 15:49:48 +1100

    changed path read form file to inline argument

--- file_dup_del.py
+++ file_dup_del.py
@@ -2,38 +2,39 @@
 this script finds and deletes duplicate files in a set of dir paths
 """
 
-from os import listdir, remove
-# from sys import argv
+from os import listdir, remove, getcwd
+from sys import argv
 
 comparison_list = []
 dir_list = []
 
 try: # if inline argument
-    with open("paths.txt", "r") as file:
-        paths = file.readlines()
+    path = argv[1]
+    # with open("paths.txt", "r") as file:
+    #     paths = file.readlines()
 except: # current dir
-    pass
+    path = getcwd()
 
-for path in paths:
+# for path in paths:
     
-    try:
-        files = listdir(path)
-    except:
-        print(f"error: {path} does not exist")
-        continue
+#     try:
+#         files = listdir(path)
+#     except:
+#         print(f"error: {path} does not exist")
+#         continue
 
-    for file in files:
+#     for file in files:
         
-        if "." in file: # if file
-            if file not in comparison_list:
-                comparison_list.append(file)
+#         if "." in file: # if file
+#             if file not in comparison_list:
+#                 comparison_list.append(file)
             
-            elif file in comparison_list:
-                print(f"{file} is a duplicate")
+#             elif file in comparison_list:
+#                 print(f"{file} is a duplicate")
             
-            elif "(" in file:
-                print(f"{file} is a duplicate")
-            # print(f"{file} is a file")
-        else: # if dir
-            dir_list.append(f"{path}\{file}")
-            # print(f"{file} is a dir")
+#             elif "(" in file:
+#                 print(f"{file} is a duplicate")
+#             # print(f"{file} is a file")
+#         else: # if dir
+#             dir_list.append(f"{path}\{file}")
+#             # print(f"{file} is a dir")
