472301522b20c5ae827f9c5ba4967f792bd6ee6a
logan harris <loganrodharris@gmail.com>
2021-03-29 22:27:37 +1100

    added proper description and help infomation

--- file_dup_del.py
+++ file_dup_del.py
@@ -3,8 +3,7 @@ This script finds and deletes duplicate files in a set of dir paths
 """
 
 from os import listdir, remove, getcwd, path
-import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 class ScanDirectory:
 
@@ -23,16 +22,20 @@ class ScanDirectory:
     def file_cycle(self, files, dir_path, switch=False):
         for file in files:
             if "." in file: # if file
-                if file not in self.comparison_list:            
+                if file not in self.comparison_list and len(file) > self.args.length:            
                     self.comparison_list.append(file)
 
-                elif file in self.comparison_list:
-                    # print(f"{file} at {dir_path} is a duplicate") # temp
-                    pass
+                elif file in self.comparison_list and len(file) > self.args.length:
+                    if self.args.notify == True:
+                        print(f"{file} at {dir_path} is a duplicate") # temp
+                    else: # delete file
+                        remove(f"{dir_path}/{file}")
 
-                elif "(" in file:            
-                    # print(f"{file} at {dir_path} is a duplicate") # temp
-                    pass
+                elif "(" in file and len(file) > self.args.length:
+                    if self.args.notify == True:
+                        print(f"{file} at {dir_path} is a duplicate") # temp
+                    else: # delete file
+                        remove(f"{dir_path}/{file}")
 
             else: # if dir
                 if switch == True:
@@ -45,10 +48,10 @@ class ScanDirectory:
         """Does the first scan in the root directory"""
 
         # gets initial path
-        if self.args.path == None: # if inline argument
+        if self.args.path == None: # if no path given
             dir_path = getcwd()
         
-        else: # current dir
+        else: # if path given
             dir_path = self.args.path
 
         try:
@@ -63,6 +66,9 @@ class ScanDirectory:
             raise Exception(f"{dir_path} does not exist")
 
         self.file_cycle(files, dir_path, switch=False)
+        # print("dir_list_1:")
+        # for i in self.dir_list_1:
+        #     print(i)
 
     def subsequent_cycle(self):
 
@@ -75,7 +81,9 @@ class ScanDirectory:
 
                     self.file_cycle(files, dir_path, switch=True)
 
-                # print(f"dir_list_2: {self.dir_list_2}\n")
+                # print("dir_list_2:")
+                # for i in self.dir_list_2:
+                #     print(i)
                 switch = False
                 self.dir_list_1.clear()
 
@@ -86,17 +94,25 @@ class ScanDirectory:
 
                     self.file_cycle(files, dir_path, switch=False)
 
-                # print(f"dir_list_1: {self.dir_list_1}\n")
+                # print("dir_list_1:")
+                # for i in self.dir_list_1:
+                #     print(i)
                 switch = True
                 self.dir_list_2.clear()
 
-parser = ArgumentParser() # need to add description
-parser.add_argument("-l", "--length", type=int, help="length of file name (including extention)")
-parser.add_argument("-p", "--path", type=str, help="the starting path (path is cwd by default")
+parser = ArgumentParser(
+    formatter_class=RawDescriptionHelpFormatter, 
+    description="""
+        Goes through the directory tree alphabetically
+        from specified root directory & deletes files with the same name
+        or files with (*) at the end."""
+        )
+parser.add_argument("-n", "--notify", action="store_true", help="only echos dulpicate files and there paths")
+parser.add_argument("-l", "--length", default=0, type=int, help="skips any file that has a name shorter than LENGTH (including extention)")
+parser.add_argument("-p", "--path", type=str, help="the starting path (path is cwd by default)")
 args = parser.parse_args()
 # print(args)
 
 scan = ScanDirectory(args)
 scan.initial_cycle()
-# print(f"dir_list_1: {scan.dir_list_1}\n")
 scan.subsequent_cycle()
